3e6f1711057f1e991155d136b8fab742da010c55
Hugo <hugo_paim@estudante.sesisenai.org.br>
2026-03-05 11:03:41 -0300

    chore: implementa ambiente compatível para ser adicionado com jetty

--- src/main/java/com/example/biblioteca/BibliotecaApplication.java
+++ src/main/java/com/example/biblioteca/BibliotecaApplication.java
@@ -2,9 +2,16 @@ package com.example.biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BibliotecaApplication {
+public class BibliotecaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BibliotecaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaApplication.class, args);
