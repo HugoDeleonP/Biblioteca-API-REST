4de9bdd21a586f65834e5e312d3ef1e7a03e0709
HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>
2026-03-06 20:33:05 -0300

    chore: configura database remoto

--- src/main/java/com/example/biblioteca/infra/DatabaseConnect.java
+++ src/main/java/com/example/biblioteca/infra/DatabaseConnect.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseConnect {
 
-    private static final String URL = "jdbc:mysql://127.0.0.1:3307/biblioteca?user=root";
+    private static final String URL = "jdbc:mysql://metro.proxy.rlwy.net:21503/railway?user=root";
     private static final String USER = "root";
-    private static final String PASSWORD = "1234";
+    private static final String PASSWORD = "kTEXaDIwwPfTKKuZOVqHOXDkhTbPMLqA";
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
